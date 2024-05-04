4e154e87cfa2eed26bd902dee163cde1c6dce322
BeingApsar <167099383+BeingApsar@users.noreply.github.com>
2024-05-04 19:39:02 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all the code as newer js version 
+
+
+let name = "apsar"  
+let age = 26
+let course;
+let isLoggedIn = false
+let state = null
+
+
+// number => 2 to power 53
+// bgint
+//string => ""
+// Boolean => true or false
+// null => Standalone value
+// undefined => value not define
+// symbol => unique
+
+
+
+Object
+
+console.log(typeof null) // it gives object
