606de0d0da60557f0270aaafaa11b5b03c3841c3
BeingApsar <167099383+BeingApsar@users.noreply.github.com>
2024-05-04 20:33:19 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,40 @@
+let score = 33
+//console.log(score);
+//console.log(typeof score)
+
+let stringScore = String(score)
+//console.log(stringScore) => 33
+//console.log(typeof stringScore) => string
+
+let  scores = "44abc"
+//console.log(scores)
+//console.log(typeof scores)
+
+let NumberIn = Number(scores)
+//console.log(NumberIn)  => Nan
+//console.log(typeof NumberIn) => this the error its type is printing Number
+
+let value = null
+//console.log(value) => null
+//console.log(typeof value) => object
+
+let values = undefined
+//console.log(values) => undefined
+//console.log(typeof values) => undefined
+
+let isLoggedIN = 0
+
+let booleanisLoggedIN = Boolean(isLoggedIN)
+
+//console.log(isLoggedIN) => 0 
+//console.log(booleanisLoggedIN) => false
+// 1 => true 
+// 0 => false
+// "apsar"=> true 
+// ""=> false
+
+
+
+
+
+
