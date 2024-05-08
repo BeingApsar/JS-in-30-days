6319ea3f60997b9f72699c64da625b24cc50bd9e
BeingApsar <167099383+BeingApsar@users.noreply.github.com>
2024-05-08 11:13:39 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,29 @@
+// console.log(2>5)  => this statement is false
+// console.log(10>3) => true
+// console.log(2==6) => false
+// console.log(2!=4) => true
+// console.log(5>=3) => true
+// console.log(6<3)  => false
+
+
+
+// console.log("2" > 1)  => in this statement changes the string into number "2" => 2
+// console.log("02">1)   => in this statement changes the string into number "2" => 2
+
+// console.log(null > 0)  => in this null is converted into desired form for this comparision , like 0 is not > 0 => false
+// console.log(null < 0)  =>false
+// console.log (null >=0) =>true
+// console.log(null <= 0) => true
+
+// console.log(0 >= 0) => true
+// console.log(0 <= 0) => true
+// console.log(0 > 0)  => false
+
+
+
+
+
+
+
+
+
